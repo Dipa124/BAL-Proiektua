2e16c3c86720f530379037aaa0ec54e42cd63c5a
Dipa124 <dipasajo@gmail.com>
2024-01-22 23:32:55 +0100

    Pasahitza sortzailea berriztu

--- Zereginen automatizazioa/Pasahitza sortzailea.py
+++ Zereginen automatizazioa/Pasahitza sortzailea.py
@@ -1,43 +1,59 @@
 import random
 import string
-from colorama import Fore, Style, Back
+import PySimpleGUI as sg
+import pyperclip as arbela
+
+# Interfaze grafikoaren konfigurazioa
+sg.theme("Reddit")
+layout = [[sg.T('PASAHITZA SORTZAILEA', font=("Segoe UI", 20, "bold"), justification='c', expand_x=True)],
+          [sg.Text("Aukeratu pasahitzaren karakter kopurua:", font=("Futura", 11))],
+          [sg.Spin(list(range(101)), expand_x=True, key="-Luzeera-"),
+           sg.Button("SORTU", key="-Sortu-", font=("Segoe UI", 9, "bold"))],
+          [sg.HorizontalSeparator(p=12)],
+          [sg.Text("Pasahitza: ", font=("Futura", 11, "bold")), sg.Input("", key="-Pasahitza-"),
+           sg.Button("KOPIATU", key="-Kopiatu-", font=("Segoe UI", 9, "bold"))],
+          [sg.Text("".upper(), key="-Oharra-")]
+          ]
+window = sg.Window("PasahitzApp", layout)
 
 
 # Pasahitza sortzen duen funtzioa
-def password_creator(num):
-    password = ""
-    for n in range(0, int(num)):
-        character_type = random.choice(("number", "letter", "symbol"))
+def pasahitza_sortzailea(luzeera):
+    pasahitza = ""
+    for z in range(0, int(luzeera)):
+        karakter_mota = random.choice(("zenbakia", "letra", "sinboloa"))
 
-        if character_type == "number":
-            character = random.choice(string.digits)
-            character = str(character)
-            password += character
-        elif character_type == "letter":
-            character = random.choice(string.ascii_letters)
-            password += character
-        elif character_type == "symbol":
-            character = random.choice(string.punctuation)
-            password += character
-    return password
+        if karakter_mota == "zenbakia":
+            karakterra = str(random.choice(string.digits))
+            pasahitza += karakterra
+        elif karakter_mota == "letra":
+            karakterra = random.choice(string.ascii_letters)
+            pasahitza += karakterra
+        elif karakter_mota == "sinboloa":
+            karakterra = random.choice(string.punctuation)
+            pasahitza += karakterra
+    return pasahitza
 
 
 # Programaren buklea
 def main():
-    print(Style.BRIGHT + Fore.CYAN + Back.LIGHTBLACK_EX +
-          "---PASAHITZA SORTZAILEA---" + Style.RESET_ALL)
-
     while True:
-        num = input("Pasahitzaren luzeera sartu (zenbaki osoetan): ")
-        try:
-            int(num)
-            break
-        except ValueError:
-            print(Style.BRIGHT + Fore.RED + "\nEmandako erantzuna ez da zuzena.",
-                  "\nZiurtatu zaitez ez duzunik zenbakia ez den bestelako karakterik",
-                  "eta sartutako zenbakia osoa dela" + Style.RESET_ALL)
+        event, value = window.read()
 
-    print("\n" + Style.BRIGHT + "Pasahitza hurrengoa da: " + Style.RESET_ALL + password_creator(num))
+        if event == sg.WIN_CLOSED or event == "-Irten-":
+            break
+        if event == "-Sortu-":
+            try:
+                luzeera = int(value["-Luzeera-"])
+                window["-Oharra-"].update("")
+                window["-Pasahitza-"].update("")
+                window["-Pasahitza-"].update(str(pasahitza_sortzailea(luzeera)))
+
+            except:
+                window["-Oharra-"].update("Emandako erantzuna ez da zuzena! Saiatu berriro", text_color="#c40000")
+        if event == "-Kopiatu-":
+            arbela.copy(value["-Pasahitza-"])
+            window["-Oharra-"].update("Pasahitza ordenagailuko arbelera kopiatu da!", text_color="#076f8c")
 
 
 # Kodea executatu
